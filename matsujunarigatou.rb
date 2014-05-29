2e5460d7674d513d91d833d5a8ff3550fa0f4f04
Hara <hara.k.aj@m.titech.ac.jp>
2014-05-30 02:00:18 +0900

    tab size

--- matsujunarigatou.rb
+++ matsujunarigatou.rb
@@ -4,36 +4,36 @@ require 'nkf'
 require 'json'
 class Matsujunarigatou
 
-	def sayHello
-		puts "Matsujun Thank You!!!"
-	end
+  def sayHello
+    puts "Matsujun Thank You!!!"
+  end
 
-	def showGoogle
-		puts NKF.nkf("-w",open("http://google.com/").read)
-	end
+  def showGoogle
+    puts NKF.nkf("-w",open("http://google.com/").read)
+  end
 
-	def showAnimeJson
-		json = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
-		#json = JSON.parser.new(html)
-		puts json
-	end
+  def showAnimeJson
+    json = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
+    #json = JSON.parser.new(html)
+    puts json
+  end
 
-	def showOneWeekFriends
-		html = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
-		json = JSON.parser.new(html)
-		hash = json.parse()
-		response = hash['response']
-		items = response['item']
+  def showOneWeekFriends
+    html = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
+    json = JSON.parser.new(html)
+    hash = json.parse()
+    response = hash['response']
+    items = response['item']
 
-		items.each do |item|
-			if item['title']=='一週間フレンズ。'
-				item.each do |key,value|
-					puts value
-				end
-			end
-		end
-		
-	end
+    items.each do |item|
+      if item['title']=='一週間フレンズ。'
+        item.each do |key,value|
+          puts value
+        end
+      end
+    end
+    
+  end
 
 end
 
