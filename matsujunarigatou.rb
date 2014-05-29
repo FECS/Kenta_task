db8123d46222b81a68a2123d6b8e706df397a5f4
Hara <hara.k.aj@m.titech.ac.jp>
2014-05-30 01:33:03 +0900

    kadai6

--- matsujunarigatou.rb
+++ matsujunarigatou.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'open-uri'
 require 'nkf'
+require 'json'
 class Matsujunarigatou
 
 	def sayHello
@@ -10,9 +11,34 @@ class Matsujunarigatou
 	def showGoogle
 		puts NKF.nkf("-w",open("http://google.com/").read)
 	end
+
+	def showAnimeJson
+		json = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
+		#json = JSON.parser.new(html)
+		puts json
+	end
+
+	def showDoraemon
+		html = NKF.nkf("-w",open('http://animemap.net/api/table/tokyo.json').read)
+		json = JSON.parser.new(html)
+		hash = json.parse()
+		response = hash['response']
+		items = response['item']
+		
+		items.each do |item|
+			if item['title']=='一週間フレンズ。'
+				item.each do |key,value|
+					puts value
+				end
+			end
+		end
+		
+	end
+
 end
 
 kenta = Matsujunarigatou.new
 
 kenta.sayHello
-kenta.showGoogle
+#kenta.showGoogle
+kenta.showDoraemon
